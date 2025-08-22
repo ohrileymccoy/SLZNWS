5e3f424378756fa25058ba7528e0a3a899a61b48
Red <YOURUSERNAME@users.noreply.github.com>
2025-08-22 12:11:38 -0400

    branding changes phase-9 of app dev from OHN

--- src/confg/site.js
+++ src/confg/site.js
@@ -1,6 +1,6 @@
 export const SITE = {
-  name: import.meta.env.VITE_SITE_NAME || 'Oak Hill News',
-  shortName: import.meta.env.VITE_SITE_SHORT || 'OHN',
-  url: import.meta.env.VITE_SITE_URL || 'https://ohn.pages.dev',
-  twitter: import.meta.env.VITE_TWITTER || '@oak_hill_news',
+  name: import.meta.env.VITE_SITE_NAME || 'Sleazy News',
+  shortName: import.meta.env.VITE_SITE_SHORT || 'SLN',
+  url: import.meta.env.VITE_SITE_URL || 'https://SLN.pages.dev',
+  twitter: import.meta.env.VITE_TWITTER || '@Sleazy_News',
 };
