15fcb975eee78318aaeb7c276bf1a6cd7483e837
Red <YOURUSERNAME@users.noreply.github.com>
2025-09-14 20:25:29 -0400

    bigger resize

--- src/main.jsx
+++ src/main.jsx
@@ -77,7 +77,7 @@ function Header() {
           <img
             src={slnLogo}
             alt="Sleazy News Logo"
-            className="h-10 w-10 object-contain"
+            className="h-15 w-15 object-contain"
           />
           <span className="font-semibold tracking-wide">
             Sleazy <span className="text-neutral-400">News</span>
