896a984e12a0f4f6ce32d303995c85144311187e
Red <YOURUSERNAME@users.noreply.github.com>
2025-09-26 22:43:36 -0400

    troubleshooting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 // tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  // make sure this covers your code
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
